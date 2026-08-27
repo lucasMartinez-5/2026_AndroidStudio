93573cc0961b1ed52f4f9d2be4dea22e3b9fb468
lucasMartinez-5 <lucasparraga1919@gmail.com>
2026-08-27 13:37:55 -0400

    firts commit

---
+++ app/src/main/java/bo/edu/uajms/lucasmartinez/a2026_TresEnRaya/MainActivity.kt
@@ -0,0 +1,11 @@
+package bo.edu.uajms.lucasmartinez.a2026_TresEnRaya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
